a619aa141f3354a73e889052b368823e30181c43
Daniel Chmur <daniel.chmur@gmail.com>
2021-09-20 16:50:43 +0200

    2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
-import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import SingleRoom from "./pages/SingleRoom";
 
 import Navbar from "./components/Navbar";
 
