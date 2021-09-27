ac9a0cc1abb456ea1535396a9379a056fb67b8cd
Daniel Chmur <daniel.chmur@gmail.com>
2021-09-27 18:22:26 +0200

    27

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -78,6 +78,21 @@ export default function RoomFilter({ rooms }) {
           </select>
         </div>
         {/* guests */}
+        {/* room price */}
+        <div className="form-group">
+          <label htmlFor="price">room price ${price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+        {/* end room price */}
       </form>
     </section>
   );
