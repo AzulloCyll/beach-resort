a9c9bb34e0d3d959a8e8502494975922eef805fa
Daniel Chmur <daniel.chmur@gmail.com>
2021-09-30 17:24:19 +0200

    fdsf

--- src/context.js
+++ src/context.js
@@ -58,7 +58,7 @@ class RoomProvider extends Component {
 
   handleChange = (event) => {
     const target = event.target;
-    const value = event.type === "checkbox" ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
 
     this.setState(
@@ -70,7 +70,8 @@ class RoomProvider extends Component {
   };
 
   filterRooms = () => {
-    let { rooms, type, capacity, price, minSize, breakfasr, pets } = this.state;
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets } =
+      this.state;
     // all the rooms
     let tempRooms = [...rooms];
     //transform value
@@ -89,6 +90,18 @@ class RoomProvider extends Component {
     //filter by price
     tempRooms = tempRooms.filter((room) => room.price <= price);
 
+    //filter by size
+    tempRooms = tempRooms.filter(
+      (room) => room.size >= minSize && room.size <= maxSize
+    );
+    //filter by breakfast
+    if (breakfast) {
+      tempRooms = tempRooms.filter((room) => room.breakfast === true);
+    }
+    //filter by pets
+    if (pets) {
+      tempRooms = tempRooms.filter((room) => room.pets === true);
+    }
     // change state
     this.setState({
       sortedRooms: tempRooms,
